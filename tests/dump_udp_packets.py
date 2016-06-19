c901f0b8be1634030939b412c0c78ad60d0fe129
Brett Graham <brettgraham@gmail.com>
2016-06-18 21:53:22 -0400

    adding display pan create to udp packet sniffer

--- tests/dump_udp_packets.py
+++ tests/dump_udp_packets.py
@@ -10,6 +10,7 @@ import flexlib
 ip = None
 port = flexlib.smartsdr.connection.DEFAULT_PORT
 udp_port = flexlib.vita.connection.DEFAULT_PORT
+#udp_port = 4991
 output_fn = '%s.json' % time.time()
 
 if ip is None:
@@ -24,17 +25,34 @@ if len(sys.argv) > 2:
 
 sdr = flexlib.smartsdr.SmartSDR(ip, port)
 sdr.send_command("client udpport %i" % udp_port)
+sdr.send_command("display pan create x=100 y=100")
+
+r = sdr.receive(timeout=0.25)
+while r is not None:
+    print r
+    for a in dir(r):
+        if a[:2] != '__':
+            print a, getattr(r, a)
+    r = sdr.receive(timeout=0.25)
 
 vita = flexlib.vita.Vita(port=udp_port)
 packets = []
+
+#cid = flexlib.vita.const.VF_METER
+cid = None
 
 print("Starting grabbing packets: Ctrl-C to stop")
 while True:
     try:
         packet = vita.receive()
         if packet is not None:
-            packets.append(packet)
-            print("Grabbed %i packets" % len(packets))
+            if packet.header.class_id.packet_class_code != cid:
+                packets.append(packet)
+                print("Grabbed %i packets" % len(packets))
+                print(packet.to_json())
+            else:
+                # print("skipping packet")
+                pass
     except KeyboardInterrupt:
         print("Stopping grabbing...")
         break
